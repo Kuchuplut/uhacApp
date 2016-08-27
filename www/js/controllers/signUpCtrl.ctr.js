948b5d3dcec38479c0e4b9046d58cb4a7f66fb9a
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2016-08-28 03:52:21 +0800

    G

--- www/js/controllers/signUpCtrl.ctr.js
+++ www/js/controllers/signUpCtrl.ctr.js
@@ -5,22 +5,38 @@
 		.module('app')
 		.controller('signupCtrl', signupCtrl);
 
-		function signupCtrl($scope, $stateParams, Account){
+		function signupCtrl($scope, $stateParams, Account, $ionicPopup){
 			var vm = this;
 			vm.details = {};
 
-				vm.signUp = function(details){
-				console.log(details);
-				console.log("Hello signup!");
-				var account 			=	new Account(details);
-				account.$save(function(data){
+			vm.signUp = function(details){
+			console.log(details);
+			console.log("Hello signup!");
+			var account 			=	new Account(details);
+			account.$save(function(data){
+				showAlert();
+				//show success message
+				//redirect to login
 
-					//show success message
-					//redirect to login
-
-				}, function(response){
-					//show error message
-				});
-			}
+			}, function(response){
+				//show error message
+			});
 		}
+
+		$scope.showAlert = function() {
+		  var alertPopup = $ionicPopup.alert({
+		    title: 'Data Registered!',
+		    template: 'Thank you for signing up!'
+		  });
+
+		  alertPopup.then(function(res) {
+		    console.log('Thank you for not eating my delicious ice cream cone');
+		    $scope.go('/homePage');
+		  });
+		 }
+
+		 $scope.go = function ( path ) {
+		   $location.path( path );
+		 };
+	}
 })();
