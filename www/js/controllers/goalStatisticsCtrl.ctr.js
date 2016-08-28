ac47d6c18c15fbdba853fd3382e5234b76965bdc
whizdummy <jeraldjohnpormon@yahoo.com>
2016-08-28 11:26:58 +0800

    fixed resource.

--- www/js/controllers/goalStatisticsCtrl.ctr.js
+++ www/js/controllers/goalStatisticsCtrl.ctr.js
@@ -5,8 +5,18 @@
 		.module('app')
 		.controller('goalStaticsCtrl', goalStaticsCtrl);
 
-		function goalStaticsCtrl($scope, $stateParams){
+		function goalStaticsCtrl($scope, $stateParams, Session, $filter, TransactionReport){
 			var vm = this;
+
+			Session.get({id : 123456789}).$promise.then(function(data){
+
+				TransactionReport.get({accountId : data.int_account_id}).$promise.then(function(trData){
+
+					vm.goalList 		=	trData.account_transaction_details;
+
+				});
+
+			});
 
 			vm.alert = function(){
 				alert("Hello");
